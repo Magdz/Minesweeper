329c6ae515bdc957475f6059588f31fe2e58f19a
Ahmed Magdy Ibrahim <magdz_008@yahoo.com>
2016-08-29 03:24:27 +0200

    Config SerialPort in Map

--- Map/Map/MainWindow.xaml.cs
+++ Map/Map/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO.Ports;
 
 namespace Map
 {
@@ -20,9 +21,33 @@ namespace Map
     /// </summary>
     public partial class MainWindow : Window
     {
+        SerialPort ArduinoSerial = new SerialPort();
+        bool run = true;
+        string COM;
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void ArduinoRead()
+        {
+            ArduinoSerial.BaudRate = 9600;
+            ArduinoSerial.PortName = COM;
+            Console.Write(COM);
+            ArduinoSerial.Open();
+
+            while (run)
+            {
+                string trigger = ArduinoSerial.ReadLine();
+            }
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            COM = COMBox.SelectedValue.ToString();
+            ArduinoRead();
+        }
+
     }
 }
